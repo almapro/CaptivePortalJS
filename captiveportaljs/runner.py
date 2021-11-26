7138a44795a2294a17207ff41d678e8a5c09c1c8
AlMA PRO <alma.pro.leader@gmail.com>
2021-11-26 17:27:25 -0500

    feat: adds basic functionality

--- captiveportaljs/runner.py
+++ captiveportaljs/runner.py
@@ -1,16 +1,31 @@
-from captiveportaljs.iptables import IPTables
-from captiveportaljs.colors import print_good, print_error
+import os
+import curses
 from time import sleep
+from captiveportaljs.curseswrapper import CursesWrapper
+from captiveportaljs.networking import Networking
+from captiveportaljs.utils import check_sudo_mode
 
 def run():
+    wrapper = CursesWrapper()
     try:
-        print('Enabling IP forwarding...')
-        IPTables.enable_ip_forwarding()
-        print_good('Enabled')
+        check_sudo_mode()
+        wrapper.print(['Available Phishing Scenarios:'], curses.A_BOLD, wrapper.main_window)
+        wrapper.print_info(['Enabling IP forwarding...'])
+        Networking.enable_ip_forwarding()
+        wrapper.print_good(['Enabled'])
+        counter = 1
         while(True):
-            print('sleeping...')
-            sleep(1)
+            wrapper.print_good(['Sleeping {}...'.format(counter)], wrapper.main_window)
+            counter += 1
+            sleep(0.5)
+            key = wrapper.main_window.getch()
+            if key == ord('\n'): break;
     except KeyboardInterrupt:
-        print_error('\n\n(^C) interrupted')
+        wrapper.print_error(['(^C) interrupted'])
     except EOFError:
-        print_error('\n\n(^D) interrupted')
+        wrapper.print_error(['(^D) interrupted'])
+    wrapper.print_info(['Disabling IP forwarding...'])
+    Networking.disable_ip_forwarding()
+    wrapper.print_good(['Disabled'])
+    curses.curs_set(True)
+    os.system('clear')
