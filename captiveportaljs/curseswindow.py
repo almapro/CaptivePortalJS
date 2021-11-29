0ffa008e8318049690cc016852a8d8e1b20d84a3
AlMA PRO <alma.pro.leader@gmail.com>
2021-11-29 09:45:43 -0500

    feat: adds logs functionality

--- captiveportaljs/curseswindow.py
+++ captiveportaljs/curseswindow.py
@@ -14,13 +14,16 @@ class CursesWindow:
         self.window.notimeout(True)
         self.window.immedok(True)
         self.window.keypad(True)
-        self.log = []
-        self.messages_log = []
-        self.devices_log = []
-        self.scroll_enabled = True
         self.focused = True
+        self.entries = []
         self.top = 0
         self.cursor = 0
+        self.scroll_enabled = True
+        self.log = []
+        self.log_top = 0
+        self.log_cursor = 0
+        self.log_scroll_enabled = True
+        self.log_shown = False
         self.draw_window_borders()
 
     def set_title(self, title):
@@ -31,6 +34,9 @@ class CursesWindow:
         # type: (bool) -> None
         self.focused = focused
         self.display()
+
+    def set_log_shown(self, log_shown):
+        self.log_shown = log_shown
 
     def resize_window(self, height, width, x, y):
         # type: (int, int, int, int) -> None
@@ -50,16 +56,40 @@ class CursesWindow:
         self.window.border()
         self.window.move(0, 1)
         self.window.move(0, int(self.window.getmaxyx()[1] / 2) - int(len(' {} '.format(self.title)) / 2))
-        self.window.addstr(' {} '.format(self.title), curses.A_BOLD if self.focused else curses.A_NORMAL)
+        if self.log_shown:
+            self.window.addstr(' LOG ({}) '.format(len(self.log)), curses.A_BOLD if self.focused else curses.A_NORMAL)
+        else:
+            self.window.addstr(' {} '.format(self.title), curses.A_BOLD if self.focused else curses.A_NORMAL)
         self.window.refresh()
 
     def display(self):
+        if self.log_shown:
+            self.display_log()
+        else:
+            self.display_entries()
+
+    def display_log(self):
         if self.help_shown: return
+        if not self.log_shown: return
         self.draw_window_borders()
         max_height = self.window.getmaxyx()[0] - 2
         max_width = self.window.getmaxyx()[1] - 2
-        log_entries = self.log[self.top:self.top + max_height]
-        for idx, item in enumerate(log_entries):
+        log = self.log[self.log_top:self.log_top + max_height]
+        for idx, item in enumerate(log):
+            text, color_pair = item
+            if idx + 1 == self.log_cursor and self.focused:
+                self.print_to_window(text.ljust(max_width), curses.color_pair(5), idx + 1)
+            else:
+                self.print_to_window(text, color_pair, idx + 1)
+
+    def display_entries(self):
+        if self.help_shown: return
+        if self.log_shown: return
+        self.draw_window_borders()
+        max_height = self.window.getmaxyx()[0] - 2
+        max_width = self.window.getmaxyx()[1] - 2
+        entries = self.entries[self.top:self.top + max_height]
+        for idx, item in enumerate(entries):
             text, color_pair = item
             if idx + 1 == self.cursor and self.focused:
                 self.print_to_window(text.ljust(max_width), curses.color_pair(5), idx + 1)
@@ -76,11 +106,11 @@ class CursesWindow:
         # type: (list[str], int) -> None
         max_lines = self.window.getmaxyx()[0] - 2
         for text in texts:
-            self.log.append([text, color_pair])
+            self.entries.append([text, color_pair])
             if self.scroll_enabled:
                 if self.cursor < max_lines:
                     self.cursor += 1
-                if len(self.log) > max_lines:
+                if len(self.entries) > max_lines:
                     self.top += 1
             self.display()
 
@@ -103,6 +133,34 @@ class CursesWindow:
     def display_string(self, window_width, target_line):
         # type: (int, str) -> str
         return target_line if window_width >= len(target_line) else target_line[:window_width]
+
+    def log_raw(self, texts, color_pair):
+        # type: (list[str], int) -> None
+        max_lines = self.window.getmaxyx()[0] - 2
+        for text in texts:
+            self.log.append([text, color_pair])
+            if self.log_scroll_enabled:
+                if self.log_cursor < max_lines:
+                    self.log_cursor += 1
+                if len(self.log) > max_lines:
+                    self.log_top += 1
+            self.display()
+
+    def log_good(self, texts):
+        # type: (list[str]) -> None
+        self.log_raw(texts, curses.color_pair(1))
+
+    def log_error(self, texts):
+        # type: (list[str]) -> None
+        self.log_raw(texts, curses.color_pair(2))
+
+    def log_warning(self, texts):
+        # type: (list[str]) -> None
+        self.log_raw(texts, curses.color_pair(3))
+
+    def log_info(self, texts):
+        # type: (list[str]) -> None
+        self.log_raw(texts, curses.color_pair(4))
 
     def show_help(self):
         self.help_shown = True
@@ -127,47 +185,101 @@ class CursesWindow:
         if key == ord('q'):
             raise GetOutOfLoop
         if key == curses.KEY_UP or key == ord('k'):
-            if self.top > 0 and self.cursor == 1: self.top -= 1
-            elif self.top > 0 or self.cursor > 1: self.cursor -= 1
-            self.scroll_enabled = False
+            if self.log_shown:
+                if self.log_top > 0 and self.log_cursor == 1: self.log_top -= 1
+                elif self.log_top > 0 or self.log_cursor > 1: self.log_cursor -= 1
+                self.log_scroll_enabled = False
+            else:
+                if self.top > 0 and self.cursor == 1: self.top -= 1
+                elif self.top > 0 or self.cursor > 1: self.cursor -= 1
+                self.scroll_enabled = False
         if key == curses.KEY_DOWN or key == curses.ACS_DARROW or key == ord('j'):
-            next_line = self.cursor + 1
-            if next_line == max_lines:
-                if self.top + max_lines < len(self.log):
-                    self.top += 1
-                else:
-                    self.cursor = next_line
-            elif next_line < max_lines:
-                if self.top + next_line < max_lines and len(self.log) > max_lines:
-                    self.cursor = next_line
-            if self.cursor > max_lines or self.cursor > len(self.log) - 3:
-                self.curor = min(len(self.log) - 3, max_lines)
-            if self.cursor == max_lines: self.scroll_enabled = True
+            if self.log_shown:
+                next_line = self.log_cursor + 1
+                if next_line == max_lines:
+                    if self.log_top + max_lines < len(self.log):
+                        self.log_top += 1
+                    else:
+                        self.log_cursor = next_line
+                elif next_line < max_lines:
+                    if self.log_top + next_line < max_lines and len(self.log) > max_lines:
+                        self.log_cursor = next_line
+                if self.log_cursor > max_lines or self.log_cursor > len(self.log) - 3:
+                    self.log_curor = min(len(self.log) - 3, max_lines)
+                if self.log_cursor == max_lines: self.log_scroll_enabled = True
+            else:
+                next_line = self.cursor + 1
+                if next_line == max_lines:
+                    if self.top + max_lines < len(self.entries):
+                        self.top += 1
+                    else:
+                        self.cursor = next_line
+                elif next_line < max_lines:
+                    if self.top + next_line < max_lines and len(self.entries) > max_lines:
+                        self.cursor = next_line
+                if self.cursor > max_lines or self.cursor > len(self.entries) - 3:
+                    self.curor = min(len(self.entries) - 3, max_lines)
+                if self.cursor == max_lines: self.scroll_enabled = True
         if key == curses.KEY_NPAGE or key == ord('K'):
-            if self.cursor == 1:
-                self.top = max(0, self.top - max_lines)
-            self.cursor = 1
-            self.scroll_enabled = False
+            if self.log_shown:
+                if self.log_cursor == 1:
+                    self.log_top = max(0, self.log_top - max_lines)
+                self.log_cursor = 1
+                self.log_scroll_enabled = False
+            else:
+                if self.cursor == 1:
+                    self.top = max(0, self.top - max_lines)
+                self.cursor = 1
+                self.scroll_enabled = False
         if key == curses.KEY_PPAGE or key == ord('J'):
-            if self.cursor == max_lines:
-                self.top += max_lines
-                if  len(self.log) - self.top == 0:
-                    self.top -= max_lines
-                elif len(self.log) - self.top > 0:
-                    self.top -= max_lines - ( len(self.log) - self.top )
-            self.cursor = min(len(self.log), min(len(self.log) + 1, max_lines))
-            if len(self.log) - self.top <= max_lines: self.scroll_enabled = True
+            if self.log_shown:
+                if self.log_cursor == max_lines:
+                    self.log_top += max_lines
+                    if  len(self.log) - self.log_top == 0:
+                        self.log_top -= max_lines
+                    elif len(self.log) - self.log_top > 0:
+                        self.log_top -= max_lines - ( len(self.log) - self.log_top )
+                self.log_cursor = min(len(self.log), min(len(self.log) + 1, max_lines))
+                if len(self.log) - self.log_top <= max_lines: self.log_scroll_enabled = True
+            else:
+                if self.cursor == max_lines:
+                    self.top += max_lines
+                    if  len(self.entries) - self.top == 0:
+                        self.top -= max_lines
+                    elif len(self.entries) - self.top > 0:
+                        self.top -= max_lines - ( len(self.entries) - self.top )
+                self.cursor = min(len(self.entries), min(len(self.entries) + 1, max_lines))
+                if len(self.entries) - self.top <= max_lines: self.scroll_enabled = True
         if key == curses.KEY_HOME or key == ord('H'):
-            self.cursor = 1
-            self.top = 0
-            self.scroll_enabled = False
+            if self.log_shown:
+                self.log_cursor = 1
+                self.log_top = 0
+                self.log_scroll_enabled = False
+            else:
+                self.cursor = 1
+                self.top = 0
+                self.scroll_enabled = False
         if key == curses.KEY_END or key == ord('E'):
-            self.cursor = min(len(self.log), max_lines)
-            self.top = max(0, min(len(self.log) - max_lines, len(self.log)))
-            self.scroll_enabled = True
+            if self.log_shown:
+                self.log_cursor = min(len(self.log), max_lines)
+                self.log_top = max(0, min(len(self.log) - max_lines, len(self.log)))
+                self.log_scroll_enabled = True
+            else:
+                self.cursor = min(len(self.entries), max_lines)
+                self.top = max(0, min(len(self.entries) - max_lines, len(self.entries)))
+                self.scroll_enabled = True
         if key == curses.KEY_DC or key == ord('C'):
-            self.log = []
-            self.cursor = self. top = 0
-            # self.top = 0
-            self.scroll_enabled = True
+            if self.log_shown:
+                self.log = []
+                self.log_cursor = self.log_top = 0
+                self.log_scroll_enabled = True
+            else:
+                self.entries = []
+                self.cursor = self.top = 0
+                self.scroll_enabled = True
+        if key == ord('L'):
+            if self.log_shown:
+                self.set_log_shown(False)
+            else:
+                self.set_log_shown(True)
         self.display()
--- captiveportaljs/runner.py
+++ captiveportaljs/runner.py
@@ -10,26 +10,43 @@ from captiveportaljs.utils import check_sudo_mode
 def scanner_thread(devices_window, stop):
     # type: (CursesWindow, Callable) -> None
     devices = []
+    disconnected_devices = []
     while True:
         if stop():
             break;
-        devices = []
-        devices_window.log.clear()
-        scan_result = Networking.range_scan('192.168.1.1/24')[0]
+        devices_window.entries.clear()
+        scan_result = Networking.range_scan('192.168.71.1/24')[0]
+        new_devices = []
         for result in scan_result:
-            devices.append('IP: {} MAC: {}'.format(result[1].psrc, result[1].hwsrc))
-        if not devices_window.help_shown:
-            devices_window.set_title('Devices on network ({})'.format(len(devices)))
-            devices_window.draw_window_borders()
-            devices_window.print_info(devices)
+            new_devices.append({ 'ip': result[1].psrc, 'mac': result[1].hwsrc })
+        reconnected_devices = [
+            device
+            for device in list(filter(lambda d: not d in new_devices, disconnected_devices))
+        ]
+        disconnected_devices = [
+            device
+            for device in list(filter(lambda d: not d in new_devices, devices))
+        ]
+        for device in new_devices:
+            if device in reconnected_devices:
+                devices_window.log_info(['Device ({}) reconnected with IP ({})'.format(device['mac'], device['ip'])])
+                devices_window.print_info(['IP: {} MAC: {} (reconnected)'.format(device['ip'], device['mac'])])
+            else:
+                devices_window.print_info(['IP: {} MAC: {}'.format(device['ip'], device['mac'])])
+        for device in disconnected_devices:
+            devices_window.log_info(['Device ({}) disconnected'.format(device['mac'])])
+            devices_window.print_error(['IP: {} MAC: {} (disconnected)'.format(device['ip'], device['mac'])])
+        devices = new_devices + disconnected_devices
+        devices_window.set_title('Devices on network ({})'.format(len(devices)))
+        devices_window.display()
         sleep(5)
 
 def curses_wrapper(screen):
     # type: (curses._CursesWindow) -> curses._CursesWindow
-    curses.init_pair(1, curses.COLOR_GREEN, screen.getbkgd())
-    curses.init_pair(2, curses.COLOR_RED, screen.getbkgd())
-    curses.init_pair(3, curses.COLOR_YELLOW, screen.getbkgd())
-    curses.init_pair(4, curses.COLOR_CYAN, screen.getbkgd())
+    curses.init_pair(1, curses.COLOR_GREEN, screen.getbkgd()) # type: ignore
+    curses.init_pair(2, curses.COLOR_RED, screen.getbkgd()) # type: ignore
+    curses.init_pair(3, curses.COLOR_YELLOW, screen.getbkgd()) # type: ignore
+    curses.init_pair(4, curses.COLOR_CYAN, screen.getbkgd()) # type: ignore
     curses.init_pair(5, curses.COLOR_BLACK, curses.COLOR_YELLOW)
     curses.cbreak()
     curses.noecho()
@@ -64,7 +81,7 @@ def run():
                 main.resize_window(max_screen_height - 5, int(max_screen_width / 2), 0, 0)
                 devices.resize_window(max_screen_height - 5, int(max_screen_width / 2), int(max_screen_width / 2), 0)
                 messages.resize_window(5 , max_screen_width, 0, max_screen_height - 5)
-            main.print_good(['Here {}'.format(counter)])
+            main.log_good(['Here {}'.format(counter)])
             counter += 1
             sleep(0.3)
             key = main.window.getch() if main.focused else messages.window.getch() if messages.focused else devices.window.getch()
