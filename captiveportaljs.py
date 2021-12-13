9edf636459b8923243d7c6641d4c15d6d5d3b3b2
AlMA PRO <alma.pro.leader@gmail.com>
2021-12-13 19:37:17 +0200

    feat: adds logging basic config

--- captiveportaljs.py
+++ captiveportaljs.py
@@ -1,5 +1,8 @@
+import logging
 import sys
 from captiveportaljs import runner
+
+logging.basicConfig(filename='captiveportal.log', encoding='utf-8', level=logging.DEBUG, force=True, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', filemode='w')
 
 try:
     raw_input # type: ignore
