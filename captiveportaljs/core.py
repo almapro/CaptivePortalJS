eb1d5b64ca770b73bad0a33cd48b8f0bd38f7a97
AlMA PRO <alma.pro.leader@gmail.com>
2021-12-13 19:33:16 +0200

    feat: adds new palette colors, event_loop variable, devices & wireless headers and handles ctrl+c exiting

--- captiveportaljs/core.py
+++ captiveportaljs/core.py
@@ -5,12 +5,17 @@ import asyncio
 from captiveportaljs.network import Network
 from captiveportaljs.window import Window
 from captiveportaljs.statusbar import StatusBar
+from captiveportaljs.wireless import Wireless, WirelessInterface
 
 class Core:
     PALETTE = [
         ('header', 'bold', ''),
-        ('item_hover', 'black', 'white'),
+        ('item_hover', 'black', 'yellow'),
         ('statusbar', 'black', 'white'),
+        ('statusbar_info', 'black', 'light cyan'),
+        ('statusbar_good', 'black', 'light green'),
+        ('statusbar_warning', 'black', 'yellow'),
+        ('statusbar_error', 'black', 'dark red'),
         ('info', 'light cyan', ''),
         ('good', 'light green', ''),
         ('warning', 'yellow', ''),
@@ -34,13 +39,30 @@ class Core:
         raise Exception('Core class is static')
 
     loop = None # type: None | urwid.MainLoop
+    event_loop = None # type: None | asyncio.AbstractEventLoop
     stop = False
 
     @staticmethod
     def start():
-        devices = Window([], [])
+        devices = Window([
+            urwid.Columns([
+                urwid.Text('IP'),
+                urwid.Text('MAC'),
+                urwid.Text('INFO'),
+            ]),
+        ], [])
         Core.set_window('devices', devices, 'Devices on network (0)')
-        wireless = Window([], [])
+        wireless = Window([
+            urwid.Columns([
+                urwid.Text('ESSID'),
+                urwid.Text('BSSID'),
+                urwid.Text('CHANNEL'),
+                urwid.Text('ENCRYPTION'),
+                urwid.Text('SIGNAL'),
+                urwid.Text('WPS'),
+                urwid.Text('CLIENTS'),
+            ]),
+        ], [])
         Core.set_window('wireless', wireless, 'Wireless Access Points/Stations (0/0)')
         messages = Window([], [])
         Core.set_window('messages', messages, 'Messages')
@@ -58,9 +80,11 @@ class Core:
         Core.set_window('main', main, 'Main')
         statusbar = StatusBar()
         active_networking_interface = Network.get_active_interface()
+        wireless_interfaces = Wireless.get_interfaces()
         networking_scanner = None # type: threading.Thread | None
+        wireless_scanner = None # type: threading.Thread | None
         if active_networking_interface:
-            Core.activate_scanner('network', blinker.signal('network'))
+            Core.activate_scanner('network', blinker.Signal())
             devices.message_info(['Enabling IP forwarding...'])
             Network.enable_ip_forwarding()
             devices.message_good(['Enabled'])
@@ -68,12 +92,28 @@ class Core:
             networking_scanner = threading.Thread(target=Network.scanner_thread, args=(devices, active_networking_interface, lambda: Core.stop))
             networking_scanner.start()
             devices.message_good(['Started'])
+            if active_networking_interface in wireless_interfaces:
+                wireless_interfaces.remove(active_networking_interface)
+        if len(wireless_interfaces) > 0:
+            Core.activate_scanner('wireless', blinker.Signal())
+            wireless.message_info(['Starting wireless scanner thread...'])
+            wireless_card = Wireless.get_interface_info(wireless_interfaces[0])
+            wireless_interface = WirelessInterface(wireless_card)
+            wireless_scanner = threading.Thread(target=Wireless.scanner_thread, args=(wireless, wireless_interface, lambda: Core.stop))
+            wireless_scanner.start()
+            wireless.message_good(['Started'])
         linebox = urwid.LineBox(urwid.Frame(main, header=urwid.Pile(main.headers), footer=statusbar), 'Main')
         loop = asyncio.get_event_loop()
+        Core.event_loop = loop
         Core.loop = urwid.MainLoop(linebox, event_loop=urwid.AsyncioEventLoop(loop=loop), unhandled_input=Core.keypress, palette=Core.PALETTE)
-        Core.loop.run()
+        try:
+            Core.loop.run()
+        except:
+            asyncio.run(Core.exit_from_core())
         if networking_scanner and Core.SCANNERS['network']:
             networking_scanner.join()
+        if wireless_scanner and Core.SCANNERS['wireless']:
+            wireless_scanner.join()
 
     @staticmethod
     def keypress(key):
@@ -81,16 +121,27 @@ class Core:
             asyncio.get_running_loop().create_task(Core.exit_from_core())
         if key == 'q':
             asyncio.get_running_loop().create_task(Core.exit_from_core())
-        if key == 'm':
+        if key == 'M':
             Core.set_active_window('main')
         if key == 'd':
             Core.set_active_window('devices')
         if key == 'w':
             Core.set_active_window('wireless')
-        if key == 'M':
+        if key == 'm':
             Core.set_active_window('messages')
         if key == 'l':
             Core.set_active_window('logs')
+        if key == 'shift tab':
+            window_to_activate = 'main'
+            windows_list = Core.WINDOWS.copy()
+            windows_list.reverse()
+            for idx, window in enumerate(windows_list):
+                if window[0] == Core.ACTIVE_WINDOW:
+                    if len(windows_list) - 1 == idx:
+                        window_to_activate = windows_list[0][0]
+                    else:
+                        window_to_activate = windows_list[idx + 1][0]
+            Core.set_active_window(window_to_activate)
         if key == 'tab':
             window_to_activate = 'main'
             for idx, window in enumerate(Core.WINDOWS):
@@ -134,7 +185,7 @@ class Core:
         # type: (str, str) -> None
         for idx, window in enumerate(Core.WINDOWS):
             if window[0] == key:
-                Core.WINDOWS[idx][3] = title
+                Core.WINDOWS[idx][2] = title
                 if Core.ACTIVE_WINDOW == key:
                     Core.set_active_window(key)
 
@@ -171,6 +222,7 @@ class Core:
     @staticmethod
     async def exit_from_core():
         Core.stop = True
+        Core.CTX.send('exiting')
         for scanner in list(Core.SCANNERS):
             if Core.SCANNERS[scanner]:
                 window = Core.get_window('messages')
@@ -185,4 +237,8 @@ class Core:
                     await asyncio.sleep(0.5)
                 if window:
                     window.entry_good(['Stopped'])
-        raise urwid.ExitMainLoop
+        try:
+            if asyncio.get_running_loop():
+                if Core.event_loop: Core.event_loop.stop()
+                raise urwid.ExitMainLoop
+        except: pass
